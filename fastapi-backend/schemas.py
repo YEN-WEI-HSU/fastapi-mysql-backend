4e1f416d728c2497c5bb9a3f66f325c5dacd4570
Mike <mike34249822@gmail.com>
2025-04-23 16:41:07 +0000

    修改點註解

--- fastapi-backend/schemas.py
+++ fastapi-backend/schemas.py
@@ -1,11 +1,11 @@
 # schemas.py   #定義資料格式
-from pydantic import BaseModel                           #引入 Pydantic 的基礎類別 `BaseModel`
+from pydantic import BaseModel                #引入 Pydantic 的基礎類別 `BaseModel`
 from typing import Literal, Optional
 
 
 # 用來描述 "data" 裡面的內容  #資料區塊（data 欄位）
 class NoteData(BaseModel):
-    uuid: Optional[str] = None  # 可選：用於 delete / update / patch
+    uuid: Optional[str] = None  # 可選：用於 delete / update / patch    # 欄位名稱: 型別 = 預設值
     title: Optional[str] = None
     content: Optional[str] = None
 
